46278d9be9cab9d8b7cd4ed7b0dd5c3fe1eac4c7
kaka.zhang <Kaka.zhang@mspbots.ai>
2022-01-19 11:20:27 +0800

    vue3

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div>
+    <div>{{ n }}</div>
+    <button @click="btnClick">+</button>
+    <HelloWorld :n="n" @setValue="setValue"></HelloWorld>
+  </div>
+</template>
+<script  lang="ts">
+import { defineComponent,reactive,watch,ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+export default defineComponent ({
+  components: {
+    HelloWorld
+  },
+  name: 'App',
+  setup() {
+    const state = reactive({ num: 10 })
+    let n = ref(1)
+    function btnClick() {
+      n.value++
+    }
+    const stop = watch(()=>n.value,(val,old)=>{
+      console.log(val,old);
+    })
+    function setValue(val) {
+      n.value = val
+    }
+    return { state,btnClick,n,setValue }
+  }
+})
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  我是子组件{{n}}
+  <button @click="updateValue">修改</button>
+</template>
+<script  lang="ts">
+import { defineComponent,reactive,watch,ref } from 'vue'
+export default defineComponent ({
+  props: ['n'],
+  setup(props,{emit}) {
+    function updateValue() {
+      emit('setValue',Math.floor(Math.random()*100))
+    }
+    return {updateValue}
+  }
+})
+</script>
